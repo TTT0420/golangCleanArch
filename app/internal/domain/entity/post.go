da9bab6e2f6dc91005b352e93ad86ea6b8987f9a
taisuke.fujii <tv0420331@gmail.com>
2024-02-23 18:25:11 +0900

    ドメインエンティティの修正

--- app/internal/domain/entity/post.go
+++ app/internal/domain/entity/post.go
@@ -1,14 +1,14 @@
-package domain
+package entity
 
 import "time"
 
 type Post struct {
-	ID          int    `jdon:"id"`
+	Id          int    `jdon:"id"`
 	UserID      int    `json:"user_id"`
 	Title       string `json:"title"`
 	Content     string `json:"content"`
 	Status      int
-	DeleteFlg   bool `json:"delete_flg"`
+	IsDeleted   bool `json:"is_deleted"`
 	CreatedDate time.Time
 	UpdatedDate time.Time
 }
