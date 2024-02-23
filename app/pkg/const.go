6bf0cbfc707f7dcfcfda1f678b35435f7f6ae5de
taisuke.fujii <tv0420331@gmail.com>
2024-02-23 22:46:45 +0900

    不要な定数を削除

--- app/pkg/const.go
+++ app/pkg/const.go
@@ -1,12 +1,7 @@
 package pkg
 
 const (
-	ResOK                   = "OK"                    //　正常時のmessageパラメーター
-	ResNG                   = "NG"                    //　失敗時のmessageパラメーター
-	ResMsg                  = "message"               //　レスポンス時のパラメーター ex) { "message": "OK" }
-	ResErr                  = "error"                 //　レスポンス時のエラーパラメーター ex) { "error": "OK" }
-	ResID                   = "id"                    // レスポンス時のIDパラメーター ex) { "message": "OK","id": 1 }
-	ResPosts                = "posts"                 // レスポンス時の投稿パラメーター ex) { "message": "OK","posts": [ {entity.Post} ] }
-	ResMsgInternalServerErr = "Internal server error" // 予期しないエラーの場合のメッセージ
-	FailedID                = -1                      // 失敗時のIDパラメーター ex) { "message": "NG","id": -1 }
+	ResOK    = "OK" //　正常時のmessageパラメーター
+	ResNG    = "NG" //　失敗時のmessageパラメーター
+	FailedID = -1   // 失敗時のIDパラメーター ex) { "message": "NG","id": -1 }
 )
