8d11463b8a062412ff62553db6010b815df30622
taisuke.fujii <tv0420331@gmail.com>
2024-02-23 18:25:34 +0900

    エラーハンドリングを追加

--- app/internal/infrastructure/router.go
+++ app/internal/infrastructure/router.go
@@ -1,16 +1,22 @@
 package infrastructure
 
 import (
+	"log"
+
 	"github.com/TTT0420/golangCleanArch/internal/infrastructure/database"
 	"github.com/TTT0420/golangCleanArch/internal/infrastructure/repository"
-	"github.com/TTT0420/golangCleanArch/internal/interface/handler"
+	"github.com/TTT0420/golangCleanArch/internal/interfaces/handler"
 	"github.com/TTT0420/golangCleanArch/internal/usecase"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
 
-	db := database.InitializeDB()
+	db, err := database.InitializeDB()
+	if err != nil {
+		log.Fatalf("DB接続失敗:%v", err)
+		return
+	}
 	postRepo := repository.NewPostRepositoryImpl(db)
 	postUsecase := usecase.NewPostUsecase(postRepo)
 	postHandler := handler.NewPostHandler(*postUsecase)
