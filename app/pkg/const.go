d4860c8e2c0969dc2a4adc0cd1e7cc5fe87dfc79
taisuke.fujii <tv0420331@gmail.com>
2024-02-23 21:32:29 +0900

    FailedIDを-1で定数化

--- app/pkg/const.go
+++ app/pkg/const.go
@@ -1,15 +1,12 @@
 package pkg
 
 const (
-	ResOK                   = "OK"
-	ResNG                   = "NG"
-	ResMsg                  = "message"
-	ResErr                  = "error"
-	ResID                   = "id"
-	ResPosts                = "posts"
-	ResMsgInternalServerErr = "Internal server error"
-)
-
-const (
-	FailedID = iota
+	ResOK                   = "OK"                    //　正常時のmessageパラメーター
+	ResNG                   = "NG"                    //　失敗時のmessageパラメーター
+	ResMsg                  = "message"               //　レスポンス時のパラメーター ex) { "message": "OK" }
+	ResErr                  = "error"                 //　レスポンス時のエラーパラメーター ex) { "error": "OK" }
+	ResID                   = "id"                    // レスポンス時のIDパラメーター ex) { "message": "OK","id": 1 }
+	ResPosts                = "posts"                 // レスポンス時の投稿パラメーター ex) { "message": "OK","posts": [ {entity.Post} ] }
+	ResMsgInternalServerErr = "Internal server error" // 予期しないエラーの場合のメッセージ
+	FailedID                = -1                      // 失敗時のIDパラメーター ex) { "message": "NG","id": -1 }
 )
