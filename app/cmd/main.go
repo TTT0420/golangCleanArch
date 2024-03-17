011aaaef56957579b311439e49c4bbfead71c566
taisuke.fujii <tv0420331@gmail.com>
2024-03-17 20:55:52 +0900

    エラー時のログ設定

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/TTT0420/golangCleanArch/internal/infrastructure"
 	"github.com/TTT0420/golangCleanArch/pkg"
 	"github.com/gin-gonic/gin"
@@ -10,6 +12,7 @@ func main() {
 	r := gin.Default()
 	logger, err := pkg.NewLogger()
 	if err != nil {
+		log.Println(pkg.LogMsgForServerError, err)
 		return
 	}
 	defer logger.Sync()
